d1ac263c45b2658cb09853a3cf785f0405a8e406
railsonweb <railsonweb@railsonweb-desktop.(none)>
2010-09-18 16:42:02 +0300

    first commit

---
+++ spec/fgraph/oauth_spec.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'spec_helper'
+
+describe FGraph::Oauth do
+  before(:each) do
+    @oauth = FGraph::Oauth.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
+
